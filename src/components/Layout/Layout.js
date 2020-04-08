ea7faaf4f049b8f384705ec5f8634a46253f113d
bBanker <fireninja171@gmail.com>
2020-04-08 09:24:46 -0400

    added testing & route guards

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxiliary';
 import './Layout.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
